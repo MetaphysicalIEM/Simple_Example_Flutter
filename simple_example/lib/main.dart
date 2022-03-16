144ceb9af8001f034b11bc2797925a6239a3dbbe
Geese_Windows_Fixe <geese.izegue@epitech.eu>
2022-03-16 22:56:54 +0100

    Dio and his friend

--- simple_example/lib/main.dart
+++ simple_example/lib/main.dart
@@ -1,115 +1,253 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+// Why Flutter
+// Flutter introduction
+// Dart language ?
+// Widget Widget ?
+// Simple example Logo + TextField + Button
+// Dio ?
+// Pros and cons about flutter my point of vieuw
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: AppMain(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AppMain extends StatefulWidget {
+  const AppMain({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<AppMain> createState() => _AppMainState();
+}
+
+class _AppMainState extends State<AppMain> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: DioPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+// https://blog.khophi.co/wp-content/uploads/2018/06/Screenshot_20180613-220644.png
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeApp> createState() => _HomeAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+TextEditingController _controllerEmail = TextEditingController();
+TextEditingController _controllerPassword = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenSizeX = MediaQuery.of(context).size.width;
+    final double screenSizeY = MediaQuery.of(context).size.height;
+    return Padding(
+      padding:
+          EdgeInsets.only(left: screenSizeX * 0.05, right: screenSizeX * 0.05),
+      child: Container(
+        width: screenSizeX,
+        height: screenSizeY,
+        color: Colors.white,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: screenSizeX * 0.12),
+              Center(
+                child: LogoApp(),
+              ),
+              SizedBox(height: screenSizeX * 0.12),
+              TextField(
+                controller: _controllerEmail,
+                keyboardType: TextInputType.phone,
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: 'Phone Number',
+                  hintStyle: TextStyle(color: Colors.black),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              TextField(
+                controller: _controllerPassword,
+                keyboardType: TextInputType.visiblePassword,
+                textAlign: TextAlign.left,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.black),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(height: screenSizeY * 0.075),
+              InkWell(
+                onTap: () {
+                  debugPrint('Log in');
+                  debugPrint(_controllerEmail.text);
+                  debugPrint(_controllerPassword.text);
+                },
+                child: Container(
+                  width: screenSizeX,
+                  height: screenSizeY * 0.05,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: screenSizeY * 0.015),
+              Container(
+                color: Colors.white,
+                width: screenSizeX,
+                height: screenSizeY * 0.05,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Forget password ?'),
+                    InkWell(
+                      onTap: () {
+                        debugPrint('forget');
+                      },
+                      child: Text(' No yawa. Tap me'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: screenSizeY * 0.05),
+              InkWell(
+                onTap: () {
+                  debugPrint('Sign up');
+                  debugPrint(_controllerEmail.text);
+                  debugPrint(_controllerPassword.text);
+                },
+                child: Container(
+                  width: screenSizeX,
+                  height: screenSizeY * 0.05,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text('No account ? Sign up'),
+                  ),
+                ),
+              ),
+            ]),
+      ),
+    );
   }
+}
+
+class LogoApp extends StatelessWidget {
+  const LogoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: NetworkImage(
+              'https://logowik.com/content/uploads/images/flutter5786.jpg'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+Future<void> dioMethod(String url) async {
+  debugPrint("GETTING DATA FROM FREE API");
+  try {
+    var response = await Dio().get(url);
+    debugPrint(response.toString());
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+}
+
+class DioPage extends StatefulWidget {
+  const DioPage({Key? key}) : super(key: key);
+
+  @override
+  State<DioPage> createState() => _DioPageState();
+}
+
+class _DioPageState extends State<DioPage> {
+  final String _urlImage =
+      'https://c.tenor.com/zly3ca4xjpEAAAAd/dio-dio-brando.gif';
+  final String _urlImage2 = 'https://c.tenor.com/Y64dVcoMZ-4AAAAC/dio-jojo.gif';
+  final String _urlImage3 =
+      'https://i.pinimg.com/originals/44/03/f8/4403f8810a593202d3b5a44893ac3835.gif';
+  final String _urlWeb = 'https://www.boredapi.com/api/activity';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.25,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage(_urlImage))),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.25,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage(_urlImage2))),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.25,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage(_urlImage3))),
+        ),
+        TextButton(
+            child: Text("DIO GET!!!!"),
+            onPressed: () async {
+              dioMethod(_urlWeb);
+            }),
+      ],
     );
   }
 }
